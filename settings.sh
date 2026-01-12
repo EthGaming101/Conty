5962ae8342e4fb386393afc9e175e541e2e54676
Extended-Tangent <86450921+EthGaming101@users.noreply.github.com>
2026-01-12 16:53:52 -0500

    doomseeker

--- settings.sh
+++ settings.sh
@@ -57,7 +57,7 @@ PACKAGES=(
 )
 
 # If you want to install AUR packages, specify them in this variable
-AUR_PACKAGES=(faugus-launcher-git gtk2)
+AUR_PACKAGES=(faugus-launcher-git gtk2 doomseeker)
 
 # ALHP is a repository containing packages from the official Arch Linux
 # repos recompiled with -O3, LTO and optimizations for modern CPUs for
